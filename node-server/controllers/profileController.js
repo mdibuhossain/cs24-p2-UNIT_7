ebcf5f4d69035d36eb55da302b5811664c1377da
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-26 20:34:52 +0600

    update profile

--- node-server/controllers/profileController.js
+++ node-server/controllers/profileController.js
@@ -1,10 +1,27 @@
+import prisma from "../db/db.config.js";
+
 export class profileController {
   static async getProfiles(req, res) {
-    // This is a placeholder for the getProfiles function
-    res.send("Get Profiles route");
+    try {
+      const findUser = req.user;
+      const { password, ...rest } = findUser;
+      res.status(200).json(rest);
+    } catch (error) {
+      res.status(500).json({ errors: error.message });
+    }
   }
   static async updateProfile(req, res) {
-    // This is a placeholder for the updateProfile function
-    res.send("Update Profile route");
+    try {
+      const findUser = req.user;
+      const { name } = req.body;
+      const updatedUser = await prisma.user.update({
+        where: { id: findUser.id },
+        data: { name },
+      });
+      const { password, ...rest } = updatedUser;
+      return res.status(201).json(rest);
+    } catch (error) {
+      res.status(500).json({ errors: error.message });
+    }
   }
 }
