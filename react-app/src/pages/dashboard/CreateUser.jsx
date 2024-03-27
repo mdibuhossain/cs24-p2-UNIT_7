19e507de15590a0b8f682c9715551a4292a00f80
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-27 15:43:19 +0600

    update

--- react-app/src/pages/dashboard/CreateUser.jsx
+++ react-app/src/pages/dashboard/CreateUser.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const CreateUser = () => {
-    return (
-        <div>
-            create new user
+  return (
+    <div className="">
+      <h2 className="mb-6 text-center text-3xl font-bold text-gray-900">
+        Create new user
+      </h2>
+      <form className="space-y-6">
+        <div className="rounded-md shadow-sm -space-y-px">
+          <div>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
         </div>
-    );
+        <div>
+          <select
+            id="role"
+            name="role"
+            className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          >
+            <option value="">Select Role</option>
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+          </select>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateUser;
