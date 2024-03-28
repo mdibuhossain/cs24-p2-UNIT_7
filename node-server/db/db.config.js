b58182d14720521c9bc23a10a6e4e8511bab800c
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-29 04:00:08 +0600

    Docker fixed

--- node-server/db/db.config.js
+++ node-server/db/db.config.js
@@ -6,33 +6,40 @@ const prisma = new PrismaClient({
 });
 
 async function main() {
-  const roles = await prisma.role.findMany();
-  if (!(roles?.length > 0)) {
-    await prisma.role.createMany({
-      data: [
-        { title: RoleType.SYSTEM_ADMIN },
-        { title: RoleType.STS_MANAGER },
-        { title: RoleType.LANDFILL_MANAGER },
-        { title: RoleType.UNASSIGNED },
-      ],
+  try {
+    const roles = await prisma.role.findMany();
+    if (!(roles?.length > 0)) {
+      await prisma.role.createMany({
+        data: [
+          { title: RoleType.SYSTEM_ADMIN },
+          { title: RoleType.STS_MANAGER },
+          { title: RoleType.LANDFILL_MANAGER },
+          { title: RoleType.UNASSIGNED },
+        ],
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    const email = "admin@admin.com";
+    const findUser = await prisma.user.findUnique({
+      where: { email },
     });
+    if (findUser) {
+      return;
+    }
+    await prisma.user.create({
+      data: {
+        name: "Admin",
+        email: "admin@admin.com",
+        password: encryptPass("admin"),
+        role: RoleType.SYSTEM_ADMIN,
+      },
+    });
+  } catch (error) {
+    console.error(error);
   }
-
-  const email = "admin@admin.com";
-  const findUser = await prisma.user.findUnique({
-    where: { email },
-  });
-  if (findUser) {
-    return;
-  }
-  await prisma.user.create({
-    data: {
-      name: "Admin",
-      email: "admin@admin.com",
-      password: encryptPass("admin"),
-      role: RoleType.SYSTEM_ADMIN,
-    },
-  });
 }
 
 main()
