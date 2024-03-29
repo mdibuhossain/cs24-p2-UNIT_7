ace2b2f0410e555289fbbeea21b49d9becda02ce
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-29 06:31:18 +0600

    update'

--- react-app/src/pages/dashboard/DashboardNav.jsx
+++ react-app/src/pages/dashboard/DashboardNav.jsx
@@ -8,6 +8,17 @@ const ADMIN_NAVs = [
   { name: "Available Roles", path: "/roles" },
   { name: "Assign Role", path: "/assign-role" },
   { name: "Create User", path: "/create-user" },
+  { name: "Create STS", path: "/create-sts" },
+  { name: "STS manager", path: "/sts-manager" },
+  { name: "Create Landfill", path: "/create-landfill" },
+  { name: "Landfill manager", path: "/landfill-manager" },
+  { name: "Add Vehicle", path: "/add-vehicle" },
+];
+
+const STS_NAVs = [{ name: "Entry vehicle", path: "/sts-vehicle-entry" }];
+
+const LANDFILL_NAVs = [
+  { name: "Entry dump record", path: "/landfill-vehicle-entry" },
 ];
 
 const COMMON_NAVs = [{ name: "Profile", path: "/profile" }];
@@ -37,17 +48,19 @@ const DashboardNav = () => {
         {/* Slidebar */}
         <div className="flex w-[280px] h-screen">
           {/* slidebar innner floating bar */}
-          <div className="p-3 m-3 w-full bg-[#1c2333] rounded-lg overflow-y-auto relative">
+          <div className="p-3 m-3 w-full bg-[#1c2333] rounded-lg overflow-y-auto relative space-y-2">
             <div className="p-1 ps-2 text-white font-extrabold text-4xl mb-10">
               EcoSync
             </div>
-            <div className="flex flex-col gap-y-2 font-light text-white">
+            <div className="flex flex-col gap-y-2 font-light text-white overflow-auto">
               {user?.role === "SYSTEM_ADMIN" ? NavListView(ADMIN_NAVs) : null}
+              {user?.role === "STS_MANAGER" ? NavListView(STS_NAVs) : null}
+              {user?.role === "LANDFILL_MANAGER" ? NavListView(LANDFILL_NAVs) : null}
               {NavListView(COMMON_NAVs)}
             </div>
             <button
               onClick={logout}
-              className="font-semibold p-1 px-3 hover:bg-gray-400 bg-gray-300 rounded-md overflow-hidden absolute bottom-3"
+              className="font-semibold p-1 px-3 hover:bg-gray-400 bg-gray-300 rounded-md overflow-hidden  bottom-3"
             >
               Logout
             </button>
