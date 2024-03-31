76ffddd225cb1ccd7d96ab4dc3136185f0971f8a
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-31 07:01:53 +0600

    update

--- node-server/routes/vehicle.router.js
+++ node-server/routes/vehicle.router.js
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { vehicleController } from "../controllers/vehicle.controller.js";
+import { roleMiddleware } from "../middlewares/role.middleware.js";
 
 const vehicleRouter = Router();
 
 vehicleRouter.get("/", authMiddleware, vehicleController.getVehicles);
-vehicleRouter.get("/sts-records", authMiddleware, vehicleController.getAllStsRecords);
-vehicleRouter.get("/dump-records", authMiddleware, vehicleController.getAllDumpRecords);
-vehicleRouter.get("/landfills/:lid/dump-records", authMiddleware, vehicleController.getSingleDumpRecords);
-vehicleRouter.get("/sts/:sid/sts-records", authMiddleware, vehicleController.getSingleStsRecords);
+vehicleRouter.get("/sts-records", authMiddleware, roleMiddleware.isAdmin, vehicleController.getAllStsRecords);
+vehicleRouter.get("/dump-records", authMiddleware, roleMiddleware.isAdmin, vehicleController.getAllDumpRecords);
+vehicleRouter.get("/landfills/:lid/dump-records", authMiddleware, roleMiddleware.isLandfillManager,vehicleController.getSingleDumpRecords);
+vehicleRouter.get("/sts/:sid/sts-records", authMiddleware, roleMiddleware.isSTSManager, vehicleController.getSingleStsRecords);
 // vehicleRouter.get("/:vid", authMiddleware, vehicleController.getVehicle);
-vehicleRouter.post("/", authMiddleware, vehicleController.createVehicle);
-vehicleRouter.post("/sts-records", authMiddleware, vehicleController.createStsRecord);
-vehicleRouter.post("/dump-records", authMiddleware, vehicleController.createDumpRecord);
+vehicleRouter.post("/", authMiddleware, roleMiddleware.isAdmin, vehicleController.createVehicle);
+vehicleRouter.post("/sts-records", authMiddleware, roleMiddleware.isSTSManager, vehicleController.createStsRecord);
+vehicleRouter.post("/dump-records", authMiddleware, roleMiddleware.isLandfillManager, vehicleController.createDumpRecord);
 
 export default vehicleRouter;
 
